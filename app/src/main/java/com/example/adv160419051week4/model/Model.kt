9f780c1e73bbf1939147d0a385be81e70c7d9881
Aditya Wijaya <85105330+aditya311200@users.noreply.github.com>
2022-03-07 15:40:41 +0700

    commit View and Model

---
+++ app/src/main/java/com/example/adv160419051week4/model/Model.kt
@@ -0,0 +1,9 @@
+package com.example.adv160419051week4.model
+
+data class Student(
+    val id:String?,
+    val name:String?,
+    val bod:String?,
+    val phone:String?,
+    val photoUrl:String?
+)
--- app/src/main/java/com/example/adv160419051week4/view/MainActivity.kt
+++ app/src/main/java/com/example/adv160419051week4/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.adv160419051week4
+package com.example.adv160419051week4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.adv160419051week4.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
