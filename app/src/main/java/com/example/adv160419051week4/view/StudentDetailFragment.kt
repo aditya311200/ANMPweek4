b9831a7e9f01b70f4ebc2f63c89948179f54fa27
Aditya Wijaya <85105330+aditya311200@users.noreply.github.com>
2022-03-08 16:21:20 +0700

    commit tugas week 4

--- app/src/main/java/com/example/adv160419051week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/adv160419051week4/view/StudentDetailFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.adv160419051week4.R
+import com.example.adv160419051week4.viewmodel.DetailViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.student_list_item.*
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewDetail:DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +25,19 @@ class StudentDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewDetail = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewDetail.fetch()
+
+        observeDetailViewModel()
+    }
+
+    fun observeDetailViewModel() {
+        viewDetail.studentLD.observe(viewLifecycleOwner, Observer {
+            txtIdDetail.setText(it.id)
+            txtNameDetail.setText(it.name)
+            txtBod.setText(it.bod)
+            txtPhone.setText(it.phone)
+        })
     }
 }
