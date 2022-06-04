a01507b52d3899bb9fff530453425ef71d47df3b
Aditya Wijaya <85105330+aditya311200@users.noreply.github.com>
2022-06-04 13:25:36 +0700

    Commit Week 11 HW (- Detail PhotoUrl)

--- app/src/main/java/com/example/adv160419051week4/view/Interfaces.kt
+++ app/src/main/java/com/example/adv160419051week4/view/Interfaces.kt
@@ -1,7 +1,12 @@
 package com.example.adv160419051week4.view
 
 import android.view.View
+import com.example.adv160419051week4.model.Student
 
 interface ButtonDetailClickListener {
     fun onButtonDetailClick(v: View)
+}
+
+interface ButtonUpdateClickListener {
+    fun onButtonUpdateClick(v: View)
 }
--- app/src/main/java/com/example/adv160419051week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/adv160419051week4/view/StudentDetailFragment.kt
@@ -5,23 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.adv160419051week4.R
+import com.example.adv160419051week4.databinding.FragmentStudentDetailBinding
+import com.example.adv160419051week4.model.Student
 import com.example.adv160419051week4.util.loadImage
 import com.example.adv160419051week4.viewmodel.DetailViewModel
 import kotlinx.android.synthetic.main.fragment_student_detail.*
 import kotlinx.android.synthetic.main.student_list_item.*
 
-class StudentDetailFragment : Fragment() {
+class StudentDetailFragment : Fragment(),
+    ButtonUpdateClickListener {
+
     private lateinit var viewDetail:DetailViewModel
+    private lateinit var dataBinding: FragmentStudentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        dataBinding = DataBindingUtil.inflate<FragmentStudentDetailBinding>(
+            inflater, R.layout.fragment_student_detail, container, false)
+
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,17 +43,25 @@ class StudentDetailFragment : Fragment() {
             viewDetail.fetch(studentId)
 
             observeDetailViewModel()
+
+            dataBinding.listener = this
         }
     }
 
     fun observeDetailViewModel() {
         viewDetail.studentLD.observe(viewLifecycleOwner, Observer {
-            txtIdDetail.setText(it.id)
-            txtNameDetail.setText(it.name)
-            txtBod.setText(it.bod)
-            txtPhone.setText(it.phone)
+            dataBinding.student = it
 
-            imageView2.loadImage(it.photoUrl.toString(), progressBar2)
+//            txtIdDetail.setText(it.id)
+//            txtNameDetail.setText(it.name)
+//            txtBod.setText(it.bod)
+//            txtPhone.setText(it.phone)
+//
+//            imageView2.loadImage(it.photoUrl.toString(), progressBar2)
         })
+    }
+
+    override fun onButtonUpdateClick(v: View) {
+        Toast.makeText(v.context, "Update Success", Toast.LENGTH_SHORT).show()
     }
 }
